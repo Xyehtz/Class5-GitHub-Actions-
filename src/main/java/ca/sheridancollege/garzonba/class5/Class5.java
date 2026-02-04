08d50327edee722ee4df3bd863fe07dc7dedd99d
Alejandro Garzon <alejitogarbo@gmail.com>
2026-02-04 14:01:44 -0500

    added basic java clasa nd test

---
+++ src/main/java/ca/sheridancollege/garzonba/class5/Class5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.garzonba.class5;
+
+/**
+ *
+ * @author aleji
+ */
+public class Class5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public String returnSomething(String message) {
+        return "Message: " + message;
+    }
+}
